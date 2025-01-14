945d406e8bcaaab5bb15781c98d7eef55bef7c97
Amadhatter03 <alex.publiccontact@gmail.com>
2025-01-13 19:29:13 -0800

    Final animation files

---
+++ ShamisenBrother1.js
@@ -0,0 +1,23 @@
+class ShamisenBrother1 {
+	constructor(game) {
+		this.game = game;
+		
+		// this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/ShamisenBrother1.png"), 0, 0, 56, 62, 4, 1);
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites/ShamisenBrother1.png"), 0, 0, 64, 64, 4, 0.2);
+
+		// Commented out code is for moving sprites (walking, flying and so forth)
+		// this.x = 0;
+		// this.y = 0;
+		// this.speed = 25;
+	};
+
+	update() {
+		// this.x += this.speed * this.game.clockTick;
+		// if(this.x < 1024) this.x = 0;
+	};
+
+	draw(ctx) {
+		// this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y, 1);
+		this.animator.drawFrame(this.game.clockTick, ctx, 250, 400, 3);
+	};
+}
--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./bear.png");
-ASSET_MANAGER.queueDownload("./snake.png");
+ASSET_MANAGER.queueDownload("./Sprites/ShamisenBrother1.png");
+ASSET_MANAGER.queueDownload("./Sprites/ShamisenBrother2.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new Bear());
-	gameEngine.addEntity(new Snake());
+	gameEngine.addEntity(new ShamisenBrother1(gameEngine));
+	gameEngine.addEntity(new ShamisenBrother2(gameEngine));
 
 	gameEngine.init(ctx);
 
